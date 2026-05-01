7374de1e3ae3fa23547020e91f0f34ff6e063c04
eltonamador <elton.amador@gmail.com>
2026-05-01 08:27:13 -0300

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import './styles/global.css'
 import Router from './app/Router.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router />
+    <Analytics />
   </StrictMode>,
 )
